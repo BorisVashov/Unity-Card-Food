bc70d1de249c04dc905a0f56a7c8e61d78ef4f61
Boris Vashov <vashovway@gmail.com>
2019-01-23 15:39:49 +0700

    balanced speed of dealing

--- Assets/Scripts/GameRules.cs
+++ Assets/Scripts/GameRules.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class GameRules
 {
-	public static readonly float DealSpeed = 5f;
+	public static readonly float DealSpeed = 15f;
 
 	public static readonly float TimeBetweenDealCard = 0.1f;
 
