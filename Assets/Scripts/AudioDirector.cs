4497d96f5d5e23baa9aa1de6d4f5e26a411b844c
Boris Vashov <vashovway@gmail.com>
2019-02-01 18:37:14 +0700

    save music status

--- Assets/Scripts/AudioDirector.cs
+++ Assets/Scripts/AudioDirector.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class AudioDirector : MonoBehaviour 
 {	
-	private bool isMusicOn = true;
+	private bool isMusicOn;
 
 	private Sprite musicOnSprite;
 	private Sprite musicOffSprite;
@@ -18,21 +18,39 @@ public class AudioDirector : MonoBehaviour
 		musicOffSprite = Resources.Load<Sprite>("UISprites/btn_mus_off");
 	}
 
+	void Start()
+	{
+		isMusicOn = bool.Parse(PlayerPrefs.GetString("music", "true"));
+		
+		UpdateMusicStatus();
+	}
+
 	public void MusicChangeButton()
+	{
+		isMusicOn = !isMusicOn;
+
+		UpdateMusicStatus();
+
+		SaveMusicStatus();
+	}
+
+	private void UpdateMusicStatus()
 	{
 		if (isMusicOn)
 		{
-			AudioListener.volume = 0;
-
-			musicButtonImage.sprite = musicOffSprite;
-			isMusicOn = false;
+			musicButtonImage.sprite = musicOnSprite;
+			AudioListener.volume = 1;
 		}
 		else
 		{
-			AudioListener.volume = 1;
-
-			musicButtonImage.sprite = musicOnSprite;
-			isMusicOn = true;
+			musicButtonImage.sprite = musicOffSprite;
+			AudioListener.volume = 0;
 		}
+	}
+
+	private void SaveMusicStatus()
+	{
+		PlayerPrefs.SetString("music", isMusicOn.ToString());
+		PlayerPrefs.Save();
 	}
 }
